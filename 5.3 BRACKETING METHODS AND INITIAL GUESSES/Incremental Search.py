b35acd7f134e91ff265eab66cf8d8cd4c095394a
lyndadream <44975450+lyndadream@users.noreply.github.com>
2020-07-21 12:54:48 +0900

    Update Incremental Search.py

--- 5.3 BRACKETING METHODS AND INITIAL GUESSES/Incremental Search.py
+++ 5.3 BRACKETING METHODS AND INITIAL GUESSES/Incremental Search.py
@@ -1,1 +1,12 @@
-#test
+#finds bracket of x that contain sign changes of a function on an interval
+#input:
+# func=name of function
+# xmin, xmax=endpoints of interval
+# ns=number of subintervals (default=50)
+#output:
+# xb(k,1) is the lower bound of the kth sign change
+# xb(k,2) is the upper bound of the kth sign change
+
+
+def incsearch(func, xmin, xmax, ns):
+    return xb
