e85ccf3b0979b5d0afd5ca5762c5fe17e6184f83
lyndadream <44975450+lyndadream@users.noreply.github.com>
2020-08-01 14:24:29 +0900

    Create 6_2_Newton-Raphson_Bungee_Jumper_Problem.py

---
+++ 6_2_Newton-Raphson_Bungee_Jumper_Problem.py
@@ -0,0 +1,16 @@
+#作成中
+
+#use the Newton-Raphson methon to estimate the root of func
+#input:
+#  func=name of function
+#  dfunc=name of derivative of function
+#  xr=initial guess
+#  es=desired relative error (default=0.0001%)
+#  maxit=maximum allowable iterations (default=50)
+#  *args=additional papameters used by function
+#output:
+#  root=real root
+#  ea=approximate relative error (%)
+#  itera=number of iterations
+def newtraph(func, dfunc, xr, es=0.0001, maxit=50, *args):
+    return root, ea, itera
